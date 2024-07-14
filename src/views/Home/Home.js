6568463ed3586b8e6f90fb3f69f874cf976f6c07
PurviMurkute <purvimurkute51@gmail.com>
2024-07-14 18:43:26 +0530

    onrender API url added

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -10,7 +10,7 @@ function Home() {
 
   const loadPlants = async () => {
     toast.loading("Loading plants...")
-        const response = await axios.get('http://localhost:8000/plants')
+        const response = await axios.get('https://nursery-client-2hle.onrender.com/plants')
 
         toast.dismiss()
         toast.success("Plants loaded successfully")
